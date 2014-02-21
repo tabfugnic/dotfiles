6b964ce569e1636b96c0abda7c46e369b5adf570
Eric Collins <tabfugnic@gmail.com>
2014-02-21 18:26:14 -0500

    Basis for i3 configuration

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,5 @@
 echo "Remove the old stuff"
-rm $HOME/.bashrc $HOME/.bash_aliases $HOME/.bash_profile $HOME/.emacs $HOME/.emacs.d
+rm -rf $HOME/.bashrc $HOME/.bash_aliases $HOME/.bash_profile $HOME/.emacs $HOME/.emacs.d $HOME/.i3*
 
 echo "Link up dotfiles"
 ln -s `pwd`/bashrc $HOME/.bashrc
@@ -7,5 +7,7 @@ ln -s `pwd`/bash_aliases $HOME/.bash_aliases
 ln -s `pwd`/bash_profile $HOME/.bash_profile
 ln -s `pwd`/emacs $HOME/.emacs
 ln -s `pwd`/emacs.d/ $HOME/.emacs.d
+ln -s `pwd`/i3/ $HOME/.i3
+ln -s `pwd`/i3status.conf $HOME/.i3status.conf
 
 echo "All done!"
