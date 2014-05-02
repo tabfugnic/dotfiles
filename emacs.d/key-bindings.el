c4e01a333e2d07612e4007e2f58336f3c88fb80e
Eric Collins <tabfugnic@gmail.com>
2014-05-02 17:51:48 -0400

    Adjust keybindings
    - Global magit keys
    - Remove word like in readline for terminal

--- emacs.d/key-bindings.el
+++ emacs.d/key-bindings.el
@@ -1,2 +1,8 @@
 ;; bind hippie-expand
-(global-set-key "\M- " 'hippie-expand)
+(global-set-key (kbd "M-SPC") 'hippie-expand)
+
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Editing
+(global-set-key (kbd "C-w") 'backward-kill-word)
