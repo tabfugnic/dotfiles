06dedbb7317894891f7ab8521e8e63ab0ed09179
Eric Collins <tabfugnic@gmail.com>
2014-06-13 12:03:40 -0400

    Update ERC to have slack

--- emacs.d/erc/erc-autojoin.el
+++ emacs.d/erc/erc-autojoin.el
@@ -8,13 +8,10 @@
 (erc-services-enable)
 (erc-autojoin-enable)
 
-(setq erc-nickserv-passwords
-      `((freenode (("tabfugnic" . ,freenode-password)))))
-
 (setq erc-autojoin-channels-alist
-      '(("thoughtbot.net" "#everyone" "#code" "#learn")
+      '(("thoughtbot" "#general")
         ("freenode.net" "#thoughtbot" "#emacs")))
 
-(erc :server "irc.thoughtbot.net" :port 6668 :nick "eric_collins" :password campfire-thoughtbot )
-(erc :server "irc.freenode.net" :port 6667 :nick "tabfugnic" )
+(erc :server "irc.freenode.net" :port 6667 :nick "tabfugnic" :password freenode-password )
+(erc-ssl :server "thoughtbot.irc.slack.com" :port 6667 :nick "eric" :password slack-thoughtbot)
 (erc-ssl :server "skilledup.irc.slack.com" :port 6667 :nick "eric" :password slack-skilledup)
--- emacs.d/erc/erc-settings-config.el
+++ emacs.d/erc/erc-settings-config.el
@@ -1,2 +1,6 @@
 (setq erc-fill-function 'erc-fill-static)
 (setq erc-fill-static-center 22)
+
+(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "MODE"))
+
+(setq erc-hide-list '("JOIN" "PART" "QUIT" "MODE"))
