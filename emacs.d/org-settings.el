0654e31e41372d13e7f7468238ae9e68eb49d44b
Eric Collins <tabfugnic@gmail.com>
2014-06-28 15:26:34 -0400

    Add methods to orgmode

--- emacs.d/org-settings.el
+++ emacs.d/org-settings.el
@@ -23,8 +23,19 @@
          (file (org-file "config.org"))
          "* %?\n")))
 
+(defun org-agenda-complete-and-archive ()
+  (interactive)
+  (org-agenda-todo 'done)
+  (org-agenda-archive-default))
+
+(defun org-complete-and-archive ()
+  (interactive)
+  (org-todo 'done)
+  (org-archive-default))
 
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+(define-key org-agenda-mode-map (kbd "C-c f") 'org-agenda-complete-and-archive)
+(define-key org-mode-map (kbd "C-c f") 'org-complete-and-archive)
