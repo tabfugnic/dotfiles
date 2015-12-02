3b4caaa019669caa47d6291772a7c933e4b4b806
Eric Collins <tabfugnic@gmail.com>
2015-12-02 10:58:47 -0500

    I like linum and I want big font, cause I'm old!

--- emacs.d/lisp/interface.el
+++ emacs.d/lisp/interface.el
@@ -10,3 +10,16 @@
 
 ;; Lines
 (global-hl-line-mode t)
+(global-linum-mode t)
+
+(defun tabfugnic/set-default-font ()
+(set-default-font "-*-DejaVu Sans Mono-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1"))
+
+(defun tabfugnic/highlight-long-lines-in-programming-modes ()
+ (require 'whitespace)
+ (setq whitespace-line-column 80)
+ (setq whitespace-style '(face lines-tail))
+ (add-hook 'prog-mode-hook 'whitespace-mode))
+
+(tabfugnic/highlight-long-lines-in-programming-modes)
+(tabfugnic/set-default-font)
