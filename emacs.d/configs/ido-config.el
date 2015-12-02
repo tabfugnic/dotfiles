59504c1f393673f95727ee696e2b9afd8f8d7e05
Eric Collins <tabfugnic@gmail.com>
2015-12-02 10:48:21 -0500

    Use C-n/p for vertical ido mode

---
+++ emacs.d/configs/ido-config.el
@@ -0,0 +1,4 @@
+(defun tabfugnic/ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'tabfugnic/ido-define-keys)
