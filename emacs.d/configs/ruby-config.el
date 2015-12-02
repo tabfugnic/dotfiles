0ae003f2a422c645d8772acf53a220c1f3e91105
Eric Collins <tabfugnic@gmail.com>
2015-12-02 11:01:29 -0500

    Remove RVM and generalize coffee
    * Remove all instances of RVM from emacs
    * I test a lot with js and coffeescript in rails

--- emacs.d/configs/ruby-config.el
+++ emacs.d/configs/ruby-config.el
@@ -1,9 +1,13 @@
+;; chruby
+(chruby "2.2.3")
+
 ;; rspec mode
 (require 'rspec-mode)
 (add-hook 'haml-mode-hook (lambda() (rspec-mode )))
 (add-hook 'html-mode-hook (lambda() (rspec-mode )))
 (add-hook 'slim-mode-hook (lambda() (rspec-mode )))
 (add-hook 'web-mode-hook (lambda() (rspec-mode )))
+(add-hook 'coffee-mode-hook (lambda() (rspec-mode )))
 
 ;; ruby mode
 (autoload 'ruby-mode "ruby-mode" "Major mode for editing ruby scripts." t)
@@ -14,13 +18,8 @@
 (setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
 (setq interpreter-mode-alist (append '(("ruby" . ruby-mode)) interpreter-mode-alist))
 
-;; RVM
-(rvm-use-default)
-(add-hook 'ruby-mode-hook (lambda () (rvm-activate-corresponding-ruby)))
-
 ;; Ruby end
 (require 'ruby-end)
-
 
 (defun rspec-toggle-spec-target-in-new-buffer
   (interactive)
