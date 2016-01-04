231bf4616b512e3883766e4fdab29d4336f63b58
Eric Collins <tabfugnic@gmail.com>
2016-01-04 15:49:22 -0500

    Auto load JSX files into JSX mode

--- emacs.d/configs/javascript-config.el
+++ emacs.d/configs/javascript-config.el
@@ -2,3 +2,7 @@
 (autoload 'javascript-mode "javascript-mode" "Javascript editing mode." t)
 (setq auto-mode-alist (cons '("\\.js$" . javascript-mode) auto-mode-alist))
 (setq js-indent-level 2)
+
+;; JSX mode
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
+(autoload 'jsx-mode "jsx-mode" "JSX mode" t)
