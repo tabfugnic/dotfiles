5bd556111116d7adc1b0d9804e05bbd658eb8cdf
Eric Collins <tabfugnic@gmail.com>
2016-07-14 00:07:39 -0400

    Update PS1

--- bash/terminal.bash
+++ bash/terminal.bash
@@ -1,2 +1,9 @@
 export TERM="xterm-256color"
-export PS1="\[\e[1;30m\][\[\e[1;30m\]\[\e[1;31m\] \`gcb\` \[\e[1;32m\]\w\[\e[0m\]\[\e[1;30m\] ]\n[ \[\e[0;33m\]\T\[\e[1;30m\] ] > \[\e[1;37m\]"
+black="\[\e[0m\]"
+gray="\[\e[1;30m\]"
+light_red="\[\e[1;31m\]"
+light_green="\[\e[1;32m\]"
+yellow="\[\e[0;33m\]"
+white="\[\e[1;37m\]"
+
+export PS1="$gray[$light_red \`gcb\` $light_green\w$black$gray ]\n[ $yellow\T$gray ] > $white"
