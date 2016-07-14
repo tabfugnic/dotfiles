cb3c76ef4bece871d89f3a65a262c02f6609bcd5
Eric Collins <tabfugnic@gmail.com>
2016-07-14 00:10:21 -0400

    Remove unnecessary time parameter

--- bash/terminal.bash
+++ bash/terminal.bash
@@ -3,7 +3,6 @@ black="\[\e[0m\]"
 gray="\[\e[1;30m\]"
 light_red="\[\e[1;31m\]"
 light_green="\[\e[1;32m\]"
-yellow="\[\e[0;33m\]"
 white="\[\e[1;37m\]"
 
-export PS1="$gray[$light_red \`gcb\` $light_green\w$black$gray ]\n[ $yellow\T$gray ] > $white"
+export PS1="$gray[$light_red \`gcb\` $light_green\W$black$gray ] > $white"
