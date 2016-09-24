744997afcf93b23c3c3b6b5d103db4d8db7ef30a
Eric Collins <tabfugnic@gmail.com>
2016-09-24 00:16:43 -0400

    Improve the look of the current branch PS1

--- bash/terminal.bash
+++ bash/terminal.bash
@@ -1,3 +1,5 @@
+. /etc/bash_completion.d/git-prompt
+
 export TERM="xterm-256color"
 black="\[\e[0m\]"
 gray="\[\e[1;30m\]"
@@ -6,8 +8,10 @@ light_green="\[\e[1;32m\]"
 light_yellow="\[\e[1;33m\]"
 white="\[\e[1;37m\]"
 
+current_branch='$(__git_ps1 "%s ")'
+
 hostname="$light_yellow\h"
-git_branch="$light_red\`gcb\`"
+git_branch="$light_red$current_branch"
 current_directory="$light_green\W"
 
 export PS1="$gray["
@@ -16,4 +20,4 @@ if [ "$SSH_CONNECTION" ]; then
     PS1="$PS1 $hostname"
 fi
 
-export PS1="$PS1 $git_branch $current_directory $gray] > $white"
+export PS1="$PS1 $git_branch$current_directory $gray] > $white"
