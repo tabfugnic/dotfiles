069864245726586d1da4eb22265f1204dc782eb1
Eric Collins <tabfugnic@gmail.com>
2016-09-23 23:37:24 -0400

    Show hostname when connected via SSH

--- bash/terminal.bash
+++ bash/terminal.bash
@@ -3,6 +3,17 @@ black="\[\e[0m\]"
 gray="\[\e[1;30m\]"
 light_red="\[\e[1;31m\]"
 light_green="\[\e[1;32m\]"
+light_yellow="\[\e[1;33m\]"
 white="\[\e[1;37m\]"
 
-export PS1="$gray[$light_red \`gcb\` $light_green\W$black$gray ] > $white"
+hostname="$light_yellow\h"
+git_branch="$light_red\`gcb\`"
+current_directory="$light_green\W"
+
+export PS1="$gray["
+
+if [ "$SSH_CONNECTION" ]; then
+    PS1="$PS1 $hostname"
+fi
+
+export PS1="$PS1 $git_branch $current_directory $gray] > $white"
