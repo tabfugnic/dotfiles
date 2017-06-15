37ebf91d2d7e71afc3d5e5787eb0032d9eb2674a
Eric Collins <tabfugnic@gmail.com>
2017-06-15 09:57:08 -0400

    Start transition off of gmail
    * Setup offlineimap to recognize new personal email account
    * Setup msmtp to recognize new personal email account
    * Change offlineimap to run in the background
    * mu4e should only attempt to run offlineimap once, maybe stop running
      it in the future

--- offlineimap/config.py
+++ offlineimap/config.py
@@ -1,24 +1,44 @@
 import subprocess
 
-FOLDER_MAP = {
+GMAIL_FOLDER_MAP = {
+    "archive": "[Gmail]/All Mail",
     "drafts":  "[Gmail]/Drafts",
     "sent":    "[Gmail]/Sent Mail",
     "trash":   "[Gmail]/Trash",
-    "archive": "[Gmail]/All Mail"
 }
 
-INVERSE_FOLDER_MAP = {v:k for k,v in FOLDER_MAP.items()}
+INVERSE_GMAIL_FOLDER_MAP = {v:k for k,v in GMAIL_FOLDER_MAP.items()}
 
-INCLUDED_FOLDERS = ["INBOX"] + FOLDER_MAP.values()
+INCLUDED_GMAIL_FOLDERS = ["INBOX"] + GMAIL_FOLDER_MAP.values()
+
+FASTMAIL_FOLDER_MAP = {
+    "archive": "INBOX.Archive",
+    "drafts":  "INBOX.Drafts",
+    "sent":    "INBOX.Sent",
+    "trash":   "INBOX.Trash",
+}
+
+INVERSE_FASTMAIL_FOLDER_MAP = {v:k for k,v in FASTMAIL_FOLDER_MAP.items()}
+
+INCLUDED_FASTMAIL_FOLDERS = ["INBOX"] + FASTMAIL_FOLDER_MAP.values()
 
 def local_folder_to_gmail_folder(folder):
-    return FOLDER_MAP.get(folder, folder)
+    return GMAIL_FOLDER_MAP.get(folder, folder)
 
 def gmail_folder_to_local_folder(folder):
-    return INVERSE_FOLDER_MAP.get(folder, folder)
+    return INVERSE_GMAIL_FOLDER_MAP.get(folder, folder)
 
-def should_include_folder(folder):
-    return folder in INCLUDED_FOLDERS
+def should_include_gmail_folder(folder):
+    return folder in INCLUDED_GMAIL_FOLDERS
+
+def local_folder_to_fastmail_folder(folder):
+    return FASTMAIL_FOLDER_MAP.get(folder, folder)
+
+def fastmail_folder_to_local_folder(folder):
+    return INVERSE_FASTMAIL_FOLDER_MAP.get(folder, folder)
+
+def should_include_fastmail_folder(folder):
+    return folder in INCLUDED_FASTMAIL_FOLDERS
 
 def find_password(name):
     try:
