782c28447b14d0517232a1f4330882868e206ff4
Eric Collins <eric@tabfugni.cc>
2021-01-19 09:21:05 -0500

    Restrict notifications in qute
    * Style it up a bit

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,3 +1,6 @@
 config.set('downloads.location.directory', '~/downloads')
+
 config.set('content.ssl_strict', True)
+config.set('content.notifications', False)
+
 config.set('tabs.background', True)
