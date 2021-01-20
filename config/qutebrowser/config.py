61045dad3e0273ab07e7655a8c2fde6a51bb5315
Eric Collins <eric@tabfugni.cc>
2021-01-20 11:20:45 -0500

    Do not ask for geolocation

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,6 +1,7 @@
 config.set('downloads.location.directory', '~/downloads')
 
-config.set('content.ssl_strict', True)
+config.set('content.geolocation', False)
 config.set('content.notifications', False)
+config.set('content.ssl_strict', True)
 
 config.set('tabs.background', True)
