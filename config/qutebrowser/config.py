1b68515f5f31c5b17de806fdf33f58224959fe47
Eric Collins <eric@tabfugni.cc>
2021-02-16 15:20:34 -0500

    Update qute configuration
    * Remove unnecessary qute script since we can do env variables in config

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,7 +1,15 @@
+config.load_autoconfig(False)
+
 config.set('downloads.location.directory', '~/downloads')
 
 config.set('content.geolocation', False)
 config.set('content.notifications', False)
 config.set('content.ssl_strict', True)
 
-config.set('tabs.background', True)
+config.set(
+    'qt.environ',
+    {
+        'QT_SCALE_FACTOR_ROUNDING_POLICY': 'PassThrough',
+        'QT_ENABLE_HIGHDPI_SCALING': '1',
+    }
+)
