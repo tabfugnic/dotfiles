5f2400327030ffeaa478267c9315f461c1300e52
Eric Collins <eric@tabfugni.cc>
2022-04-06 13:20:25 -0400

    Default to kitty terminal

--- bash/terminal.bash
+++ bash/terminal.bash
@@ -1,18 +1,18 @@
 . /etc/bash_completion.d/git-prompt
 
-export TERM="xterm-256color"
-black="\[\e[0m\]"
-gray="\[\e[1;30m\]"
-light_red="\[\e[1;31m\]"
-light_green="\[\e[1;32m\]"
-light_yellow="\[\e[1;33m\]"
+export TERMINAL="kitty"
+export TERM="kitty"
+
+red="\[\e[0;31m\]"
+green="\[\e[0;32m\]"
+yellow="\[\e[0;33m\]"
 white="\[\e[0;37m\]"
 
 current_branch='$(__git_ps1 "%s ")'
 
-hostname="$light_yellow\h"
-git_branch="$light_red$current_branch"
-current_directory="$light_green\W"
+hostname="$yellow\h"
+git_branch="$red$current_branch"
+current_directory="$green\W"
 
 PS1="";
 
@@ -20,4 +20,4 @@ if [ "$SSH_CONNECTION" ]; then
   PS1="$PS1 $hostname"
 fi
 
-export PS1="$PS1 $git_branch$current_directory $gray> $white"
+export PS1="$PS1 $git_branch$current_directory $white> "
