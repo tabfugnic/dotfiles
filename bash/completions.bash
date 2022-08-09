b8aceb72b4740caa384b9d8073a50dbb27cd196f
Eric Collins <eric@tabfugni.cc>
2022-08-09 10:41:43 -0400

    Make sure pass-otp uses completions

--- bash/completions.bash
+++ bash/completions.bash
@@ -5,5 +5,6 @@ check_and_source() {
 }
 check_and_source /usr/share/bash-completion/completions/git
 check_and_source /usr/share/bash-completion/completions/pass
+check_and_source /usr/share/bash-completion/completions/pass-otp
 
 unset -f check_and_source
