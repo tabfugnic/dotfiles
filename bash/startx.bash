a0a886a13d597950824781399316ba3a2c6397cd
Eric Collins <eric@tabfugni.cc>
2025-03-04 22:32:51 -0500

    Convert to Sway from i3wm
    Using wayland now instead of X11 with some x11 backups
    
    It runs mostly seemlessly but required fixing some of the ways
    programs were called.
    
    * Using swaylock config to ensure consistent lock screen
    * Using output settings in sway to change how monitor displays content
    * Remove redundant code(locker)
    * Remove xmodmap
    * start sway from bash

--- bash/startx.bash
+++ bash/startx.bash
@@ -1,5 +1,5 @@
 # Start i3
-command -v startx &> /dev/null &&
-  [[ -z $DISPLAY ]] &&
-  [[ $(tty) = /dev/tty1 ]] &&
-  exec startx
+# command -v startx &> /dev/null &&
+#   [[ -z $DISPLAY ]] &&
+#   [[ $(tty) = /dev/tty1 ]] &&
+#   exec startx
